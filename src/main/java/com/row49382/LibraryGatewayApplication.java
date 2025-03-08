28253a67ae36459d7071a520d234c246e19507d2
row49382 <r-wroblewski@bethel.edu>
2025-03-07 23:04:07 -0600

    first commit

---
+++ src/main/java/com/row49382/LibraryGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.row49382;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryGatewayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/row49382/row49382_library_gateway/LibraryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.row49382.row49382_library_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
